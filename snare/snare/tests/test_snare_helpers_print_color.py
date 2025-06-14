b60dd2491dc425e70b17f57da36ff8985e529a6f
yehezkiel1086 <tsukikagemaikaka1@gmail.com>
2025-06-14 14:27:10 +0700

    feat: update snare

--- snare/snare/tests/test_snare_helpers_print_color.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from snare.utils.snare_helpers import print_color
-
-
-class TestPrintColor(unittest.TestCase):
-    def test_print_color(self):
-        self.assertIsNone(print_color("testing print_color()", "INFO"))
-        self.assertIsNone(print_color("testing print_color()", "WRONG_MODE"))
